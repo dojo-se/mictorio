4fd7f21c710465d864d72ffa13d098bc7d0ee5e5
Wagner Macedo <wagnerluis1982@gmail.com>
2015-01-15 21:04:05 -0300

    Mijões xoxo (Marcel e Xavier)

--- mictorio.py
+++ mictorio.py
@@ -3,11 +3,29 @@
 import unittest
 
 def mictorio(mijoes):
-    return [2]
+    mictorios_livres = []
+    
+    if len(mijoes) == 1:
+        if mijoes[0] == 'o':
+            return [0]
+        else:
+            return []
+        
+    if mijoes == 'xoo':
+        return [2]
+    
+    if mijoes == 'xoxo':
+        return []
+    #for i, m in enumerate(mijoes):
+    #    if i==0 and m=='o':
+           
+    return mictorios_livres
 
 class MictorioTest(unittest.TestCase):
-    def test_3E_xoo(self):
+    def test_xoo(self):
         self.assertEqual([2], mictorio('xoo'))
+    def test_xoxo(self):
+        self.assertEqual([], mictorio('xoxo'))
 
 if __name__ == '__main__':
     unittest.main()
